94584b6718bc0ca61a713bba3710a57338ad30b6
Sebastian Grebe <sebigrebe@googlemail.com>
2020-01-12 12:31:50 +0100

    mongoose user

--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,12 +1,13 @@
 import { Field, ID, ObjectType } from 'type-graphql';
 import { buildSchema } from '@typegoose/typegoose';
+import { Document } from 'mongoose';
 
 /**
  * TypeScript User class
  * Uses decorators from type-graphql package to allow automatic generation of GraphQL schema
  */
 @ObjectType()
-export class User {
+export class User extends Document {
   @Field(type => ID)
   userId!: string|number;
 
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,34 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { User } from './users.model';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class UsersService {
-  private readonly users: User[];
-
-  // @todo Get users from database
-  constructor() {
-    this.users = [
-      {
-        userId: 1,
-        username: 'john',
-        password: 'changeme',
-      },
-      {
-        userId: 2,
-        username: 'chris',
-        password: 'secret',
-      },
-      {
-        userId: 3,
-        username: 'maria',
-        password: 'guess',
-      },
-    ];
-  }
+  constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
 
   // @todo Rewrite to get user from database
   // @todo Extend query parameters
-  async findOne(username: string): Promise<User | undefined> {
-    return this.users.find(user => user.username === username);
+  async findOne(username: string): Promise<User | null> {
+    return this.userModel.findOne({username: username});
   }
 }
