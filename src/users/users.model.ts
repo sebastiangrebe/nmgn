28f61fec26f33563e7905e35ecea3ba71dd203ec
Sebastian Grebe <sebigrebe@googlemail.com>
2020-01-12 14:34:31 +0100

    fix mongoose document

--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,21 +1,25 @@
 import { Field, ID, ObjectType } from 'type-graphql';
-import { buildSchema } from '@typegoose/typegoose';
-import { Document } from 'mongoose';
+import { buildSchema, prop, modelOptions, getModelForClass } from '@typegoose/typegoose';
 
 /**
  * TypeScript User class
  * Uses decorators from type-graphql package to allow automatic generation of GraphQL schema
  */
+@modelOptions({ schemaOptions: {} })
 @ObjectType()
-export class User extends Document {
+export class User {
+  @prop()
   @Field(type => ID)
   _id!: string|number;
 
+  @prop()
   @Field()
   username!: string;
 
+  @prop()
   @Field()
   password?: string;
 }
 
+export const UserModel = getModelForClass(User);
 export const UserSchema = buildSchema(User);
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,15 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import { User } from './users.model';
-import { Model } from 'mongoose';
+import { User, UserModel } from './users.model';
 import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
+  constructor(@InjectModel('User') private readonly userModel: typeof UserModel) {}
 
-  // @todo Rewrite to get user from database
   // @todo Extend query parameters
   async findOne(username: string): Promise<User | null> {
-    return this.userModel.findOne({username: username});
+    return this.userModel.findOne({username: username}).lean();
   }
 }
